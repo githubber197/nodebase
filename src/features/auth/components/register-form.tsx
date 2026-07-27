2bd9a2d69b3382adb521ae2a5e584c06feaf054c
Rajeev <168348784+githubber197@users.noreply.github.com>
2026-07-27 21:38:34 +0530

    ai-providers

--- src/features/auth/components/register-form.tsx
+++ src/features/auth/components/register-form.tsx
@@ -88,6 +88,7 @@ export function RegisterForm() {
                                         type="button"
                                         disabled={isPending}
                                     >
+                                        <Image alt="Github" src="/logos/github.svg" width={20} height={20}/>
                                         Continue with GitHub
                                     </Button>
                                     <Button 
@@ -96,6 +97,7 @@ export function RegisterForm() {
                                         type="button"
                                         disabled={isPending}
                                     >
+                                        <Image alt="Google" src="/logos/google.svg" width={20} height={20}/>
                                         Continue with Google
                                     </Button>
                                 </div>
--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,14 +1,30 @@
 
-import { baseProcedure, createTRPCRouter, protectedProcedure } from '../init';
+import { inngest } from '@/inngest/client';
+import {  createTRPCRouter, protectedProcedure } from '../init';
 import prisma from '@/lib/db';
+import { email } from 'zod';
+import { google } from '@ai-sdk/google';
+import { generateText } from 'ai';
+
+
 export const appRouter = createTRPCRouter({
-    getUsers: protectedProcedure.query(({ ctx }) => {
-      //console.log({ userId: ctx.auth.user.id });
-      return prisma.user.findMany({
-        where: {
-          id: ctx.auth.user.id,
-        }
-      });
+  testAI: protectedProcedure.mutation( async () => {
+    await inngest.send({
+      name: "execute/ai"
+    });
+    return { success: true, message: "Job queued"}
+  }),
+    getWorkflows: protectedProcedure.query(({ ctx }) => {
+      return prisma.workflow.findMany();
+      }),
+      createWorkflow: protectedProcedure.mutation(async () => {
+        await inngest.send({
+          name: "test/processTask",
+          data: {
+            email: "rajeev@gmail.com"
+          },
+        });
+        return { success: true, message: "Job queued"}
     }),
 });
  
